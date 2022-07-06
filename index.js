44539b6c7c060f7f2c03b2be6a20ebb06dd867ed
Bartosz Leper <bleper@google.com>
2022-07-06 12:17:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  res.send(`Hello ${name}! How are you?`);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
